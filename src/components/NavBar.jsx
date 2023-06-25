cb1e76ac77c666582390e9436e40d08dfef7a92c
Gab <gab@gab.art.br>
2023-06-25 16:14:48 -0300

    fix: update realtive path

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,14 +11,14 @@ export const NavBar = ({ setSearchTerm, setSelectedGenre }) => {
   return (
     <div className="navbar">
       <div className="logo">
-        <img src="/logo.svg" alt="logo" />
+        <img src="./logo.svg" alt="logo" />
       </div>
       <div className="logo-mobile">
-        <img src="/icon.svg" alt="icon" />
+        <img src="./icon.svg" alt="icon" />
       </div>
       <div className="search-bar">
         <input type="text" placeholder='Search by Game Title' onChange={handleSearchChange} />
-        <img className='search-icon' src="/search.svg" alt="" />
+        <img className='search-icon' src="./search.svg" alt="" />
       </div>
     </div>
   )
