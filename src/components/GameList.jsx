283a576a2a8070486f8bb127682d4b2585aafc79
Gab <gab@gab.art.br>
2023-07-14 22:00:42 -0300

    refactor: improve functionality of components

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
+import Select, { components } from 'react-select';
 import { getDoc, doc, setDoc } from "firebase/firestore";
 import { auth, db } from "../services/firebaseConfig";
 import { NavBar } from './NavBar';
-import { SearchFilter } from './SearchFilter';
 import { Card } from "./Card";
+import searchIcon from "../assets/search.svg";
 import './GameList.css'
 
 export const GameList = ({ gameList }) => {
@@ -14,6 +15,80 @@ export const GameList = ({ gameList }) => {
   const [filteredData, setFilteredData] = useState(gameList);
   const [userData, setUserData] = useState(null);
   const [userDocId, setUserDocId] = useState(null);
+
+  const colourStyles = {
+    control: (styles, state) => ({ 
+      ...styles, 
+      width: '160px',
+      backgroundColor: 'rgb(26, 31, 43)',
+      color: 'rgba(246, 248, 251, 0.87)',
+      fontSize: '0.8rem',
+      fontWeight: 400,
+      boxShadow: state.isFocused ? '0 0 0 1px rgb(100, 108, 255)' : '',
+      border: '1px solid rgba(138, 148, 168, 0.5)',
+      borderRadius: '0.5rem',
+    }),
+    option: (base, state) => ({
+      ...base,
+      color: state.isFocused ? 'rgb(255, 255, 255)' : 'rgba(246, 248, 251, 0.87)',
+      backgroundColor: state.isFocused ? 'rgba(26, 31, 43, 1)' : 'rgba(26, 31, 43, 0.7)',
+      borderBottom: '0.05rem solid rgba(246, 248, 251, 0.05)',
+      cursor: 'pointer',
+      fontSize: '0.8rem',
+      fontWeight: 400,
+    }),
+    singleValue: (styles) => ({
+      ...styles,
+      color: 'rgba(246, 248, 251, 0.87)',
+    }),
+    menu: (provided) => ({
+      ...provided,
+      backgroundColor: 'rgba(26, 31, 43, 0.9)',
+    }),
+  };
+
+  const uniqueGenres = ['All Games', ...new Set(gameList?.map((game) => game.genre))];
+
+  const genreCount = {};
+  gameList.forEach((game) => {
+    if (genreCount[game.genre]) {
+      genreCount[game.genre] += 1;
+    } else {
+      genreCount[game.genre] = 1;
+    }
+  });
+
+  genreCount['All Games'] = gameList.length;
+
+  const genreOptions = uniqueGenres.map(genre => ({ 
+    value: genre, 
+    label: genre,
+  }));
+
+  const CustomOption = ({ data, ...props }) => (
+    <components.Option {...props}>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        {data.label}
+        <span style={{ backgroundColor: 'rgb(100, 108, 255)', borderRadius: '4px', padding: '0 5px' }}>
+          {genreCount[data.value]}
+        </span>
+      </div>
+    </components.Option>
+  );
+
+  const genreFilter = (selectedOption) => {
+    if (selectedOption.value === 'All Games') {
+      setFilteredData(gameList);
+    } else {
+      setFilteredData(gameList.filter((game) => game.genre === selectedOption.value))
+    }
+    setSelectedGenre(selectedOption);
+  };
+
+  const handleSearchChange = (e) => {
+    const searchTerm = e.target.value;
+    setSearchTerm(searchTerm);
+  };
   
   const checkUserDoc = async () => {
     const userUID = auth?.currentUser?.uid;
@@ -60,13 +135,26 @@ export const GameList = ({ gameList }) => {
   return (
     <>
       <NavBar />
-      <SearchFilter
-        genreList={gameList}
-        setFilteredData={setFilteredData}
-        setSelectedGenre={setSelectedGenre}
-        selectedGenre={selectedGenre}
-        setSearchTerm={setSearchTerm}
-      />
+
+      <div className="search-filter">
+        <div className="search-bar">
+          <input type="text" placeholder='Search by game title' onKeyDown={handleSearchChange} />
+          <img className='search-icon' src={searchIcon} alt="" />
+        </div>
+
+        <div className="genre-list">
+          <Select 
+            styles={colourStyles}
+            options={genreOptions}
+            value={selectedGenre}
+            onChange={genreFilter}
+            isSearchable={false}
+            placeholder='Genre'
+            components={{ Option: CustomOption }}
+          />
+        </div>
+      </div>
+      
       <div className='gameList'>
         <div className="container-list">
         {filteredData?.map((game) => (
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,33 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import Select from "react-select";
+import { useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { auth } from "../services/firebaseConfig";
 import { Button } from "./Button";
 import logoImg from "../assets/logo.svg";
 import icon from "../assets/icon.svg";
 import avatar from "../assets/avatar.svg";
-import downArrow from "../assets/down-arrow.svg";
 import './NavBar.css';
 
 export const NavBar = () => {
 
+  const navigate = useNavigate();
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
   const user = auth?.currentUser;
+
+  const options = [
+    { value: "favorites", label: "Favorites" },
+    { value: "logout", label: "Logout" },
+  ];
+
+  const handleSelectChange = (selectedOption) => {
+    if (selectedOption.value === "logout") {
+      handleLogout();
+    } else if (selectedOption.value === "favorites") {
+      navigate('/favorites');
+    }
+  };
 
   console.log(auth?.currentUser?.uid);
 
@@ -26,10 +40,6 @@ export const NavBar = () => {
     }
   };
 
-  const handleDropdownClick = (e) => {
-    e.stopPropagation();
-  };
-
   return (
     <div className="navbar">
       <div className="logo">
@@ -38,27 +48,50 @@ export const NavBar = () => {
       <div className="logo-mobile">
         <img src={icon} alt="icon" />
       </div>
-      <div className="dropdown">
+      <div>
         {user ? (
-          <div className="user-info" onClick={toggleDropdown}>
+          <div className="user-info">
             <img src={avatar} alt="User Avatar" />
-            <span>{user?.email}</span>
-            <img src={downArrow} alt="Dropdown Arrow" />
-            {dropdownOpen && (
-              <ul className="dropdown-menu" onClick={handleDropdownClick}>
-                <li>
-                  <Link to="/favorites">Favorites</Link>
-                </li>
-                <li>
-                  <button onClick={handleLogout}>Logout</button>
-                </li>
-              </ul>
-            )}
+            <Select
+              options={options}
+              onChange={handleSelectChange}
+              isSearchable={false}
+              placeholder={user.email}
+              styles={{
+                container: (base) => ({
+                  ...base,
+                  fontSize: '0.8rem',
+                  fontWeight: 300,
+                }),
+                option: (base, state) => ({
+                  ...base,
+                  color: state.isFocused ? 'rgba(246, 248, 251, 0.67)' : 'rgba(246, 248, 251, 0.87)',
+                  backgroundColor: state.isFocused ? 'rgb(17, 21, 29)' : 'rgb(26, 31, 43)',
+                  borderTop: '0.05rem solid rgba(246, 248, 251, 0.1)',
+                  cursor: 'pointer',
+                }),
+                placeholder: (base) => ({
+                  ...base,
+                  fontSize: '0.8rem',
+                  color: 'rgba(246, 248, 251, 0.87)',
+                  fontWeight: 300,
+                }),
+                control: (baseStyles, state) => ({
+                  ...baseStyles,
+                  backgroundColor: 'rgb(26, 31, 43)',
+                  group: 'rgb(26, 31, 43)',
+                  background: state.menuIsOpen ? 'rgb(26, 31, 43)' : '',
+                  borderColor: state.isFocused ? 'grey' : '',
+                  border: 'none',
+                  cursor: 'pointer',
+                }),
+              }}
+            />
           </div>
         ) : (
-          <div>
-            <Button><Link to="/auth?signup=true">Sign Up</Link></Button>
-            <Button><Link to="/auth">Login</Link></Button>
+          <div className="login-area">
+            <Button classType={'secondary-navbar-button'} onClickFunction={() => {navigate('/auth')}}>Login</Button>
+            <Button classType={'primary-navbar-button'} onClickFunction={() => {navigate('/auth?signup=true')}}>Sign Up</Button>
           </div>
         )}
       </div>
