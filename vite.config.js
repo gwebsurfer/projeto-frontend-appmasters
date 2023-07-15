ce8f51d86c01cc084e1edd91734585783e454208
Gab <gab@gab.art.br>
2023-07-15 20:35:16 -0300

    chore: update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/projeto-frontend-appmasters",
+  base: "/projeto-frontend-appmasters/",
   build: {
     chunkSizeWarningLimit: 1000,
   }
